21a7fea3d19cfdb6f007a30867b9b4666cfae112
Tiago Sampaio <tiago@tiagosampaio.com>
2020-02-14 21:19:24 -0300

    Improvements.

---
+++ Block/Html/Advisor/TopHeader.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace MagedIn\LoginAsCustomer\Block\Html\Advisor;
+
+/**
+ * Class TopHeader
+ *
+ * @package MagedIn\LoginAsCustomer\Block\Html\Advisor
+ */
+class TopHeader extends \Magento\Framework\View\Element\Template
+{
+
+}
